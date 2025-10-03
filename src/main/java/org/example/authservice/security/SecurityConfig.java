fed299cafd2dafb82a6cd9196d932600350a6c86
alyvnihad <aliyevnihad011@gmail.com>
2025-10-03 19:44:27 +0400

    Security configuration add commit - Auth Service

---
+++ src/main/java/org/example/authservice/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package org.example.authservice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.example.authservice.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity httpSecurity, UserService userService, PasswordEncoder passwordEncoder) throws Exception {
+        AuthenticationManagerBuilder managerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        managerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder);
+        return managerBuilder.build();
+    }
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth
+                .requestMatchers("api/auth/register", "api/auth/login").permitAll()
+                .requestMatchers("api/auth/logout").authenticated()
+                .anyRequest().authenticated()
+        ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
