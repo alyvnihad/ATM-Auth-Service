57eaad7a16db6b4404d481440120243215d90a2e
alyvnihad <aliyevnihad011@gmail.com>
2025-10-06 19:33:07 +0400

    Config update.v2 commit - Auth Service

--- src/main/java/org/example/authservice/config/GeneralConfig.java
+++ src/main/java/org/example/authservice/config/GeneralConfig.java
@@ -2,7 +2,7 @@ package org.example.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,7 +11,7 @@ public class GeneralConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+        return new Argon2PasswordEncoder( 16, 32, 1, 65536, 3 );
     }
 
     @Bean
