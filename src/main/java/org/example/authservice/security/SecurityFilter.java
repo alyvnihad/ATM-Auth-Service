1a0c8b858a991fc9cd1bf4c9debb27e82815f5bb
alyvnihad <aliyevnihad011@gmail.com>
2025-10-06 19:34:24 +0400

    SecurityFilter update.v1 commit - Auth Service

--- src/main/java/org/example/authservice/security/SecurityFilter.java
+++ src/main/java/org/example/authservice/security/SecurityFilter.java
@@ -1,17 +1,17 @@
 package org.example.authservice.security;
 
-import de.mkammerer.argon2.Argon2;
-import de.mkammerer.argon2.Argon2Factory;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class SecurityFilter {
 
-    private final Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+    private final PasswordEncoder passwordEncoder;
 
-    public String passwordEncoder(String pin){
-        return argon2.hash(4,65536,2,pin);
+    public String Encoder(String pin){
+        return passwordEncoder.encode(pin);
     }
 }
