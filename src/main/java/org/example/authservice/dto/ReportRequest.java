e6ef5c0ef75bf91f932b9a113976b50b17a9b9b3
alyvnihad <aliyevnihad011@gmail.com>
2025-10-23 11:34:49 +0400

    ReportRequest add dto class - Auth Service

---
+++ src/main/java/org/example/authservice/dto/ReportRequest.java
@@ -0,0 +1,16 @@
+package org.example.authservice.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class ReportRequest {
+    private Long accountNumber;
+    private String name;
+    private String email;
+    private Long cardNumber;
+    private LocalDate expiryDate;
+    private String currency;
+    private String paymentNetwork;
+}
